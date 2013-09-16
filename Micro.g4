e8857419967c83a8dd2b6626b260be67b092bc55
alancs23 <alancse17@gmail.com>
2013-09-15 22:20:14 -0400

    updated grammar for parser

--- Micro.g4
+++ Micro.g4
@@ -1,33 +1,54 @@
 grammar Micro;
 a : 'a' ;
-KEYWORD :  'PROGRAM' 
-        |  'BEGIN'
-        |  'STRING'
-        |  'FUNCTION'
-        |  'INT'
-        |  'FLOAT'
-        |  'IF'
-        |  'ELSIF'
-        |  'ENDIF'
-        |  'END'
-        |  'VOID'
-        |  'DO'
-        |  'WHILE'
-        |  'WRITE'
-        |  'RETURN'
-        |  'READ'
-        |  'TRUE'
-        |  'BREAK'
-        ;
+
+//keywords
+PROGRAM : 'PROGRAM' ;
+BEGIN   : 'BEGIN'   ;
+STRING  : 'STRING'  ;
+FUNCTION :  'FUNCTION' ;
+INT     :  'INT' ;
+FLOAT   :  'FLOAT' ;
+IF      :  'IF' ;
+ELSIF   :  'ELSIF' ;
+ENDIF   :  'ENDIF' ;
+END     :  'END' ;
+VOID    :  'VOID' ;
+DO      :  'DO' ;
+WHILE   :  'WHILE' ;
+WRITE   :  'WRITE' ;
+RETURN  :  'RETURN' ;
+READ    :  'READ' ;
+TRUE    :  'TRUE' ;
+BREAK   :  'BREAK' ;
+
+//variable
 IDENTIFIER : [_A-Za-z]+[0-9]* ;
 INTLITERAL : [0-9]+ ;
 FLOATLITERAL: [-]*[0-9]*[.][0-9]+ ;
-OPEARTOR : [\+\-\*\/\(\)\{\}\<\>\!\,\;\=\.] 
-         | ':='
-         | '!='
-         | '<='
-         ;
 STRINGLITERAL : '"'~["]*'"' ;
+
+//operator
+PLUS : '+' ;
+MINUS : '-';
+MUL : '*' ;
+DIV : '/' ;
+LPAREN : '(' ;
+RPAREN : ')' ;
+LBRACE : '{' ;
+RBRACE : '}' ;
+LABRACKET : '<' ;
+RABRACKET : '>' ;
+COMMA : ',' ;
+EXCLAMATION : '!' ;
+SEMICOLON : ';' ;
+EQUAL : '=' ;
+DOT : '.' ;
+ASSIGN : ':=' ;
+NEQUAL : '!=' ;
+LEQUAL : '<=' ;
+GEQUAL : '>=' ;
+
+
 COMMENT : '-''-'~[\n]* -> skip ;
 WS : [ \t\r\n]+ -> skip ;
 
--- build/MicroParser.java
+++ build/MicroParser.java
@@ -14,11 +14,20 @@ public class MicroParser extends Parser {
 	protected static final PredictionContextCache _sharedContextCache =
 		new PredictionContextCache();
 	public static final int
-		T__0=1, KEYWORD=2, IDENTIFIER=3, INTLITERAL=4, FLOATLITERAL=5, OPEARTOR=6, 
-		STRINGLITERAL=7, COMMENT=8, WS=9;
+		T__0=1, PROGRAM=2, BEGIN=3, STRING=4, FUNCTION=5, INT=6, FLOAT=7, IF=8, 
+		ELSIF=9, ENDIF=10, END=11, VOID=12, DO=13, WHILE=14, WRITE=15, RETURN=16, 
+		READ=17, TRUE=18, BREAK=19, IDENTIFIER=20, INTLITERAL=21, FLOATLITERAL=22, 
+		STRINGLITERAL=23, PLUS=24, MINUS=25, MUL=26, DIV=27, LPAREN=28, RPAREN=29, 
+		LBRACE=30, RBRACE=31, LABRACKET=32, RABRACKET=33, COMMA=34, EXCLAMATION=35, 
+		SEMICOLON=36, EQUAL=37, DOT=38, ASSIGN=39, NEQUAL=40, LEQUAL=41, GEQUAL=42, 
+		COMMENT=43, WS=44;
 	public static final String[] tokenNames = {
-		"<INVALID>", "'a'", "KEYWORD", "IDENTIFIER", "INTLITERAL", "FLOATLITERAL", 
-		"OPEARTOR", "STRINGLITERAL", "COMMENT", "WS"
+		"<INVALID>", "'a'", "'PROGRAM'", "'BEGIN'", "'STRING'", "'FUNCTION'", 
+		"'INT'", "'FLOAT'", "'IF'", "'ELSIF'", "'ENDIF'", "'END'", "'VOID'", "'DO'", 
+		"'WHILE'", "'WRITE'", "'RETURN'", "'READ'", "'TRUE'", "'BREAK'", "IDENTIFIER", 
+		"INTLITERAL", "FLOATLITERAL", "STRINGLITERAL", "'+'", "'-'", "'*'", "'/'", 
+		"'('", "')'", "'{'", "'}'", "'<'", "'>'", "','", "'!'", "';'", "'='", 
+		"'.'", "':='", "'!='", "'<='", "'>='", "COMMENT", "WS"
 	};
 	public static final int
 		RULE_a = 0;
@@ -78,8 +87,8 @@ public class MicroParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\uacf5\uee8c\u4f5d\u8b0d\u4a45\u78bd\u1b2f\u3378\3\13\7\4\2\t\2\3\2"+
-		"\3\2\3\2\2\3\2\2\2\5\2\4\3\2\2\2\4\5\7\3\2\2\5\3\3\2\2\2\2";
+		"\3\uacf5\uee8c\u4f5d\u8b0d\u4a45\u78bd\u1b2f\u3378\3.\7\4\2\t\2\3\2\3"+
+		"\2\3\2\2\3\2\2\2\5\2\4\3\2\2\2\4\5\7\3\2\2\5\3\3\2\2\2\2";
 	public static final ATN _ATN =
 		ATNSimulator.deserialize(_serializedATN.toCharArray());
 	static {
