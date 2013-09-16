68b89b18bf826266b310f86d37c401fc2c722351
alancs23 <alancse17@gmail.com>
2013-09-15 22:19:23 -0400

    update grammar for parser

--- src/Micro.java
+++ src/Micro.java
@@ -7,8 +7,10 @@ public class Micro {
     public static void main(String[] args) throws IOException {
 	ANTLRInputStream input = new ANTLRInputStream(new FileReader(args[0]));
 	MicroLexer lexer = new MicroLexer(input);
-	while(true) {
-	    Token tokens = lexer.nextToken();
+	CommonTokenStream tokens = new CommonTokenStream(lexer);
+	MicroParser p = new MicroParser(tokens);
+
+	    /*
 	    if(tokens.getType() == -1) {
 		break;
 	    }
@@ -18,30 +20,7 @@ public class Micro {
 	    case 2:
 		System.out.println("Token Type: KEYWORD");
 		System.out.println("Value: " + tokens.getText());
-		break;
-	    case 3:
-		System.out.println("Token Type: IDENTIFIER");
-		System.out.println("Value: " + tokens.getText());
-		break;
-	    case 4:
-		System.out.println("Token Type: INTLITERAL");
-		System.out.println("Value: " + tokens.getText());
-		break;
-	    case 5:
-		System.out.println("Token Type: FLOATLITERAL");
-		System.out.println("Value: " + tokens.getText());
-		break;
-	    case 6:
-		System.out.println("Token Type: OPERATOR");
-		System.out.println("Value: " + tokens.getText());
-		break;
-	    case 7:
-		System.out.println("Token Type: STRINGLITERAL");
-		System.out.println("Value: " + tokens.getText());
-		break;
+		} */
 
-	    }
-
-	}
     }
 }
